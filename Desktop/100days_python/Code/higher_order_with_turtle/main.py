1bccca0cad06bc5ec0197940b38c09294356c7ba
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-04-14 16:44:41 -0600

    etch a sketch game made

--- Desktop/100days_python/Code/higher_order_with_turtle/main.py
+++ Desktop/100days_python/Code/higher_order_with_turtle/main.py
@@ -5,11 +5,31 @@ tammy = Turtle()
 screen = Screen()
 
 
+def move_forwards():
+    tammy.forward(10)
 
 
+def move_backwards():
+    tammy.backward(10)
+    
+    
+def move_left():
+    tammy.left(10)
+    
 
-
-
-
-
-screen.listn()
+def move_right():
+    tammy.right(10)
+    
+    
+def clear():
+    tammy.clear()
+    # tammy.reset()
+    
+    
+screen.listen()
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(clear, "c")
+screen.exitonclick()
